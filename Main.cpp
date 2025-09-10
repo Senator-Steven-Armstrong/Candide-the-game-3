832de28dc1f99b9c1da1fb50925e485297038d83
Senator Armstrong <loke.lindstrom@outlook.com>
2025-09-10 16:30:13 +0200

    man what the fuck

--- Main.cpp
+++ Main.cpp
@@ -3,4 +3,24 @@
 int main() {
     std::cout << "Hello, World!";
     int i = 0; // Adds Herobrine
+
+
+    // Start funktion som körs när spelet startas
+        // Meny
+
+    // Main loop
+        //
+
 }
+
+void render() {
+
+}
+
+class Tomas {
+    public:
+        int hp = 100;
+        int atk = 10;
+        int sta = 50;
+        int dex = 10;
+};
